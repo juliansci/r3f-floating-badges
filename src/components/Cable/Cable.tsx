807da337b994fe3a5bbade7f6fc1e009a0180ce4
Juliansci <julian.scialabba@gmail.com>
2023-04-12 23:04:31 -0300

    Fix build

--- src/components/Cable/Cable.tsx
+++ src/components/Cable/Cable.tsx
@@ -1,14 +1,14 @@
 import { QuadraticBezierLine } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
-import { useRef } from "react";
+import { MutableRefObject, useRef } from "react";
 function Cable({
   start,
   end,
   v1 = new THREE.Vector3(),
   v2 = new THREE.Vector3(),
 }: any) {
-  const ref = useRef<any>();
+  const ref = useRef<any>(null);
 
   useFrame(() => {
     const position1 = start.current.getWorldPosition(v1);
@@ -17,9 +17,17 @@ function Cable({
       new THREE.Vector3(position1.x, position1.y, position1.z - 3),
       new THREE.Vector3(position2.x, position2.y, position2.z - 3)
     );
-  }, []);
+  });
 
-  return <QuadraticBezierLine ref={ref} lineWidth={3} color="#ff2060" />;
+  return (
+    <QuadraticBezierLine
+      ref={ref}
+      lineWidth={3}
+      color="#ff2060"
+      start={[0, 0, 0]}
+      end={[0, 0, 0]}
+    />
+  );
 }
 
 export default Cable;
